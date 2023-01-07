44194e880ee4e840d3bb81c73adaf62d3d0c2008
Alexander Winter <a.w1nter@hotmail.com>
2023-01-07 00:08:39 -0500

    Not working but compile LUL if any issue contact cedricmartens98@gmail.com

--- client/include/world/Asteroid.h
+++ client/include/world/Asteroid.h
@@ -14,37 +14,38 @@
 
 struct VecCompare {
     bool operator() (const sf::Vector2f& lhs, const sf::Vector2f& rhs) const {
-      return lhs.x < rhs.x || lhs.x == rhs.x && lhs.y < rhs.y;
+        return lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y);
     }
 };
 
 class Asteroid : public Entity {
-	mutable sf::Sprite sprite;
-	
-	float size;
-	
-	float rotation;
-	
+    mutable sf::Sprite sprite;
+    mutable sf::Sprite zoneSprite;
+    
+    float size;
+    
+    float rotation;
+    
   sf::Vector2f velocity;
-	float angularVelocity;
+    float angularVelocity;
 
   std::map<sf::Vector2f, Crop*, VecCompare> plantingZones;
 
 public:
-	constexpr static const float MAX_SIZE = 10.0f;
-	
-	explicit Asteroid(Space& space,
-					  const sf::Vector2f& location,
-	                  float rotation,
-	                  float size,
-	                  const sf::Vector2f& startVelocity,
-	                  float startAngVelocity);
+    constexpr static const float MAX_SIZE = 10.0f;
+    
+    explicit Asteroid(Space& space,
+                      const sf::Vector2f& location,
+                      float rotation,
+                      float size,
+                      const sf::Vector2f& startVelocity,
+                      float startAngVelocity);
 
-	void tick(float delta) override;
+    void tick(float delta) override;
 
-	void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override;
+    void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override;
 
-  float getZOrder() const override;
+    float getZOrder() const override;
 
 private:
     void generatePlantingZones();
