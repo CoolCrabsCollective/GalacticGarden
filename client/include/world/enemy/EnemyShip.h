3b2e873190b73c8cb82d321392e44f1801f71fa8
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-06 23:46:20 -0500

    Damage flower when you get close to it

--- client/include/world/enemy/EnemyShip.h
+++ client/include/world/enemy/EnemyShip.h
@@ -12,6 +12,7 @@ protected:
     void draw(sf::RenderTarget &target, const sf::RenderStates &states) const override;
     mutable sf::Sprite sprite;
     float speed;
+    int damage;
 public:
     explicit EnemyShip(Space& space, sf::Vector2f location);
     void tick(float delta) override;
--- client/src/world/crop/FalloutFlower.cpp
+++ client/src/world/crop/FalloutFlower.cpp
@@ -12,6 +12,7 @@ float FalloutFlower::get_time_to_maturity() const {
 
 FalloutFlower::FalloutFlower(Space &space, sf::Vector2f location) : Crop(space, location) {
     sprite.setTexture(*space.getAssets().get(GameAssets::TEXTURE_FALLOUT_FLOWER));
+    health = 10;
 }
 
 float FalloutFlower::getZOrder() const {
