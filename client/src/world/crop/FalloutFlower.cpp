0d1e8c6fe086f13359d06063f99aee708db455c2
Adrien Tremblay <adrien@adrientremblay.com>
2023-01-07 10:37:47 -0500

    tweak hatchling damage math

--- client/src/world/crop/FalloutFlower.cpp
+++ client/src/world/crop/FalloutFlower.cpp
@@ -12,7 +12,7 @@ float FalloutFlower::get_time_to_maturity() const {
 
 FalloutFlower::FalloutFlower(Space &space, sf::Vector2f location) : Crop(space, location) {
     sprite.setTexture(*space.getAssets().get(GameAssets::TEXTURE_FALLOUT_FLOWER));
-    health = 500.0f;
+    health = 5.0f;
 }
 
 float FalloutFlower::getZOrder() const {
