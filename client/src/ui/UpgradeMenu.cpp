6bb240c0de7ac53c43e49d3d4e405c2e516e8325
Cedric Martens <cedricmartens98@gmail.com>
2023-01-08 13:07:26 -0500

    Optimized upgrade menu

--- client/src/ui/UpgradeMenu.cpp
+++ client/src/ui/UpgradeMenu.cpp
@@ -9,14 +9,16 @@
 UpgradeMenu::UpgradeMenu(Space& space, UpgradeManager &upgradeManager) : space(space), upgradeManager(upgradeManager) {
 
     wiz::AssetLoader& ass = space.getAssets();
-    upgrade_texture[Upgrade::LASER_DOUBLE] = ass.get(GameAssets::TEXTURE_LASER_DOUBLE);
-    upgrade_texture[Upgrade::LASER_TRIANGLE] = ass.get(GameAssets::TEXTURE_LASER_TRIANGLE);
-    upgrade_texture[Upgrade::LASER_FOUR_WAY] = ass.get(GameAssets::TEXTURE_LASER_FOUR_WAY);
-    upgrade_texture[Upgrade::LASER_CRAZY] = ass.get(GameAssets::TEXTURE_LASER_CRAZY);
-    upgrade_texture[Upgrade::NUKE_SIMPLE] = ass.get(GameAssets::TEXTURE_NUKE_SIMPLE);
-    upgrade_texture[Upgrade::FLAMETHROWER_SIMPLE] = ass.get(GameAssets::TEXTURE_FLAMETHROWER_SIMPLE);
-    upgrade_texture[Upgrade::BOOST_BASIC] = ass.get(GameAssets::TEXTURE_BOOST_BASIC);
-    upgrade_texture[Upgrade::BOOST_ULTRA] = ass.get(GameAssets::TEXTURE_BOOST_ULTRA);
+    upgrade_sprites[Upgrade::LASER_DOUBLE] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_LASER_DOUBLE)};
+    upgrade_sprites[Upgrade::LASER_TRIANGLE] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_LASER_TRIANGLE)};
+    upgrade_sprites[Upgrade::LASER_FOUR_WAY] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_LASER_FOUR_WAY)};
+    upgrade_sprites[Upgrade::LASER_CRAZY] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_LASER_CRAZY)};
+    upgrade_sprites[Upgrade::NUKE_SIMPLE] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_NUKE_SIMPLE)};
+    upgrade_sprites[Upgrade::FLAMETHROWER_SIMPLE] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_FLAMETHROWER_SIMPLE)};
+    upgrade_sprites[Upgrade::BOOST_BASIC] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_BOOST_BASIC)};
+    upgrade_sprites[Upgrade::BOOST_ULTRA] = new sf::Sprite{*ass.get(GameAssets::TEXTURE_BOOST_ULTRA)};
+
+    locked.setTexture(*ass.get(GameAssets::TEXTURE_LOCKED));
 }
 
 
@@ -24,7 +26,6 @@ void UpgradeMenu::draw(sf::RenderTarget &target, const sf::RenderStates &states)
     float offsetX = 0.f;
     float xPos = 50.f;
     float yPos = 1000.f;
-    sprite.setScale({2.f, 2.f}); // make alex cringe
 
     for(const auto& upgradeLine : upgradeTree)
     {
@@ -44,16 +45,24 @@ void UpgradeMenu::draw(sf::RenderTarget &target, const sf::RenderStates &states)
             const std::vector<Upgrade>& upgradeBlock = upgradeLine.at(index);
             for(int i = 0; i < upgradeBlock.size(); i++)
             {
-                sf::Texture* texture = upgrade_texture.at(upgradeBlock[index]);
-                sprite.setTexture(*texture);
-                sprite.setPosition({xPos + offsetX, yPos});
-                target.draw(sprite);
+                sf::Sprite* sprite = upgrade_sprites.at(upgradeBlock[index]);
+                sprite->setPosition({xPos + offsetX, yPos});
+                sprite->setScale({2.f, 2.f}); // will make alex cringe
+                target.draw(*sprite);
+                locked.setPosition(sprite->getPosition());
+                locked.setScale(sprite->getScale());
+                if(upgradeManager.get_cost(upgradeBlock[index]) > space.getShip().getEnergy())
+                {
+                    target.draw(locked);
+                }
+
                 sf::Text price;
                 int price_value = upgradeManager.get_cost(upgradeBlock[index]);
                 price.setString(std::to_string(price_value));
                 price.setPosition({xPos + offsetX + 2, yPos + 30.f});
                 price.setFont(*space.getAssets().get(GameAssets::VT323_TTF));
                 target.draw(price);
+
                 offsetX += 100;
             }
         }
